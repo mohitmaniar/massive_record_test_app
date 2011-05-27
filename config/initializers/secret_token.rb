4590c7c1615863fdefab282184cfbc8ca57f49ed
Thorbjørn Hermansen <thhermansen@gmail.com>
2011-05-27 20:06:24 +0200

    Added a new Rails 3.1 RC app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MassiveRecordTestApp::Application.config.secret_token = '5e02bfe3e6f18ef6fce55e56d5f4194baf8c963943c5c165f0142f02c934cae31098e467345cd4a88cc52c7f6ceb7e7fc265dfc1d3f8f8a1058a5a514409ce86'
